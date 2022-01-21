d0f21eebfa1e8d07ea033e88121752298c319053
nev.kd2832@gmail.com <nev.kd2832@gmail.com>
2022-01-21 07:38:10 +0300

    changed logic of how password gen works

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,5 @@
 // Assignment Code
-let encrypted_pass={};  //final password
-
 var generateBtn = document.querySelector("#generate");
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
 // Write password to the #password input
@@ -12,86 +7,75 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  // const passwordBox = document.getElementById("passwordBox");
-  const length = document.getElementById("length");
-  let password = "";
-  while ((encrypted_pass).length.value > passwordText.length) {
-    let keyToAdd = getKey[Math.floor(Math.random() * getKey.length)];
-    let isChecked = document.getElementById(keyToAdd.name).checked;
-    if (isChecked) {
-      password += keyToAdd();
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", passwordCriteria);
+
+
+// Password Criteria Function 
+function passwordCriteria() {
+    let start = prompt("You will be presented witha series of prompts for password criteria")
+    var characters = prompt("What is the length of your password? Minimum 8 and maximum 128 characters.");
+    if(isNaN (parseInt(characters))) {
+    alert("Please input a number between 8 and 128.")
+        return
+    } else if(parseInt(characters) < 8 || parseInt(characters) > 128) {
+    alert("Sorry, your password is either too short or too long. Please keep your password between 8-128 characters in length.");
+    return;
     }
 
-}
+    var passwordOptions = ""
 
-// Password Generation Function
-function generatePassword(){
-  var result = window.confirm("You will receive a series of prompts to construct your password.");
-  console.log(result);
 
-  // decides whether user wants to go ahead with password gen or not
-  if (result===true){
-    passwdLength()
-  } else{
-    return("User didn't continue with password generation.");
-  }
-}
-
-// Password Criteria Functions
-
-// Criteria 1: Password Length
-function passwdLength(){
-
-    // lets the user choose the password length
-    let passwd_length = window.prompt("Choose length of password from 8 characters to 128 characters:","Example: 8, 50, 78, etc.");
-    console.log(passwd_length);
-    encrypted_pass.length = passwd_length;
-
-    // checks if length has a data type of number
-    if((isNaN(passwd_length))===false){
-      return charType();
-    } else{
-      // Alerts user to re-enter a valid input
-      window.alert("Invalid Input! Type a number below:")
-      return passwdLength();
+    var lowerCase = confirm("Would you like lower case letters in your password?");
+    if(lowerCase === true) {
+    var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+    passwordOptions += lowerCaseLetters
     }
-}
 
-// Criteria 2: Password Character Types
-// Passwd Char Types Object
-const types = {
-  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  lowerCase: "abcdefghijklmnopqrstuvwxyz",
-  number: "0123456789",
-  special: "!@#$%^&*()_+~\\`|}{[]:;?><,./-="
-}
+    var upperCase = confirm("Would you like upper case letters in your password?");
+    if(upperCase === true) {
+        var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        passwordOptions += upperCaseLetters
+        }
 
-// Array
-const getKey = [
-  function upperCase() {
-    return keys.upperCase[Math.floor(Math.random() * types.upperCase.length)];
-  },
-  function lowerCase() {
-    return keys.lowerCase[Math.floor(Math.random() * types.lowerCase.length)];
-  },
-  function number() {
-    return keys.number[Math.floor(Math.random() * types.number.length)];
-  },
-  function symbol() {
-    return keys.symbol[Math.floor(Math.random() * types.special.length)];
-  }
-];
 
-// Validation of Passwd Char Types
-function charType(){
-  let upper = window.confirm("Do you want your password to have 'UPPERCASE' characters");
-  let lower = window.confirm("Do you want your password to have 'LOWERCASE' characters");
-  let number = window.confirm("Do you want your password to have 'NUMBER' characters");
-  let special = window.confirm("Do you want your password to have 'SPECIAL' characters");
+    var numbers = confirm("Would you like numbers in your password?");
+    if(numbers === true) {
+    var numbersTrue = "1234567890"
+    passwordOptions += numbersTrue
+    }
 
-  // checks if at least one character type is chosen
-  if (upper + lower + number + special === 0) {
-    alert("Please check atleast one box!");
-    return charType();
-  }
-}
+
+    var symbols = confirm("Would you like symbols in your password? Special characters include: !@#$%^&*()_+");
+    if(symbols === true) {
+    var symbolsTrue = '"~!@#$%^&*()_+{}|"'
+    passwordOptions += symbolsTrue
+    }
+
+
+    var password = ""
+
+
+    // console.log(characters);
+    // console.log(lowerCase);
+    // console.log(upperCase);
+    // console.log(numbers);
+    // console.log(symbols);
+    console.log(parseInt (characters))
+    // Validity check on variables
+    // passwordOptions holds the user's password criteria.
+    console.log(passwordOptions)
+
+    // Randomly choose characters from passwordOptions
+    for (var i = 0; i < characters; i++) {
+    var randomNumber = Math.floor(Math.random() * passwordOptions.length);
+    password += passwordOptions.substring(randomNumber, randomNumber +1);
+    }
+    // password pulls from passwordOptions to create the user's password.
+    document.getElementById("password").value = password;
+    console.log(password);
+    }
